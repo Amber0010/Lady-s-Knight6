d357f9d202cce50680b5cadbe26a9d79672487d0
NicoleTD <nicoletd.amsterdam@gmail.com>
2026-02-10 15:34:51 -0500

    InitialGame+2Scripts

---
+++ Assets/Scripts/Environtment/Rock.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject sirRoly;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (sirRoly.GetComponent<SirRoly_Special>().IsRolled() == true)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Players/LadyBug_Special.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class LadyBug_Special
+{
+    
+}
