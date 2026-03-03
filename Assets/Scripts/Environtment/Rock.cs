22d30caa0ada94b755d9ebe0d83cc745fbf2dad9
Amber0010 <nicoleterdoest@ufl.edu>
2026-03-03 15:59:38 -0500

    Force this

--- Assets/Scripts/Environtment/Rock.cs
+++ Assets/Scripts/Environtment/Rock.cs
@@ -19,7 +19,7 @@ public class Rock : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        //if (sirRoly.GetComponent<SirRoly_Special>())
-            //Destroy(this.gameObject);
+        if (sirRoly.GetComponent<SirRoly_Movement>())
+            Destroy(this.gameObject);
     }
 }
