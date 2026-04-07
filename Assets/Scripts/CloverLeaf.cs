64b1131f0c55288bef4ff9f9112681c3bbca78e0
Amber0010 <nicoleterdoest@ufl.edu>
2026-04-07 15:56:20 -0400

    Revert "Merge branch 'Christiana' into MergeBranch2"
    This reverts commit a0cf684f52d94a59d88ce4c0f8b5e924edd8e360, reversing
    changes made to efbab82a7a25b242adf6fef5a2e4787e4ed9d903.

--- Assets/Scripts/CloverLeaf.cs
+++ Assets/Scripts/CloverLeaf.cs
@@ -3,16 +3,13 @@ using UnityEngine;
 
 public class CloverLeaf : MonoBehaviour, ICollection
 {
-    public AudioClip myClipClover;
-    AudioSource audioSource;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public static event Action<int> OnCloverCollect;
         public void Collect()
     {
         OnCloverCollect?.Invoke(1);
-        audioSource = GetComponent<AudioSource>();
-        audioSource.PlayOneShot(myClipClover);
         Destroy(gameObject);
     }
 }
